b2eb800a9ad7491e48420f299639b8a24d7210ef
Chrislee <christoisuta06@gmail.com>
2025-04-27 15:29:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="vieport" constant=width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?= WELCOME TO TV TEKNOLOGI !"; ?>
-    </body>
-    </html>TESHDJASHDKSHDAKD
-    tugas ini di buat chris t
+tugas ini di buat chris t
